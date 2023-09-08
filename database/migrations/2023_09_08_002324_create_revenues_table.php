38b6c76ea6e85b5feeddedc9424199f3ee8e362e
sakaidubz <sakai.s.am@m.titech.ac.jp>
2023-09-08 10:25:05 +0900

    Migration and install Breeze

--- database/migrations/2023_09_08_002324_create_revenues_table.php
+++ database/migrations/2023_09_08_002324_create_revenues_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('revenues', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description', 140);
+            $table->decimal('amount');
+            $table->string('type');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('revenues');
     }
 };
---
+++ database/migrations/2023_09_08_002352_create_entry_fee_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entry_fee', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('ticket_price');
+            $table->integer('number_of_guests');
+            $table->decimal('total_revenue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entry_fee');
+    }
+};
