0d9b3ecf33522cc2c4b32effb0b34a55d8815190
sakaidubz <sakai.s.am@m.titech.ac.jp>
2023-11-11 23:07:30 +0900

    Update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,11 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+        
+        <div class="flex items-center justify-center mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Register') }}
+            </a>
+        </div>
     </form>
 </x-guest-layout>
